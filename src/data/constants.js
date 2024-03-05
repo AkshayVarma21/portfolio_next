bec296885a771fc63c4a17820012d1f89a92b1c7
Akshay <varma.akshay121@gmail.com>
2024-03-05 05:45:21 +0530

    resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "As a UI developer, I specialize in creating engaging user interfaces that seamlessly blend functionality with aesthetic appeal.",
   github: "",
   resume:
-    "https://drive.google.com/file/d/1Um5cXUMevp0mczHpEUfzp78RC4KnYlAY/view?usp=drive_link",
+    "https://drive.google.com/file/d/1Um5cXUMevp0mczHpEUfzp78RC4KnYlAY/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/akshay-varma-749126178/",
   mail: "https://www.google.com/gmail/about/",
   mailId:"varma.akshay121@gmail.com"
